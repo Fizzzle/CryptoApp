2a34b420df23d2e3652e278e6bc16ec481f3c456
Viktor <Rickeygame23@gmail.com>
2022-12-07 19:56:02 +0200

    1

---
+++ app/src/main/java/com/example/cryptoapp/pojo/Datum.kt
@@ -0,0 +1,8 @@
+package com.example.cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Datum (@SerializedName("CoinInfo")
+                  @Expose
+                  val CoinInfo: CoinInfo? = null,)
